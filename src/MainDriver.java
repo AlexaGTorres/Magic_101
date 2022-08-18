92420f314cd1d7868be947f9b6ab748e8640a6a6
AlexaGTorres <alexa.torres19122694@gmail.com>
2022-08-18 12:19:14 -0700

    Asking user to pick a number.

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class MainDriver
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner (System.in);//Create scanner object
+        System.out.println("Welcome! Pick a character based on the numbers!" +
+                "\n1: Ice" +
+                "\n2: Fire" +
+                "\n3: Electric");
 
+
+        int userSel = scan.nextInt();//Get user number input
+        while (userSel != 1 && userSel != 2 && userSel != 3)
+        {
+
+        }
+
+        System.out.println("Test: " + userSel);
     }
 }//End class MainDriver
